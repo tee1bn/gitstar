6a516cadaad30682fb61f766b80ba58ab8684708
Taiwo <tee02bn@gmail.com>
2020-07-22 16:58:14 +0100

    commit

--- template/default/guest/includes/header.php
+++ template/default/guest/includes/header.php
@@ -59,13 +59,13 @@ $address = '<address>17, Isikalu lane, Apapa, Lagos, Nigeria</address>
         <i class="fa fa-envelope-o"></i> <a href="mailto:<?=$email;?>"><?=$email;?></a>
         <i class="fa fa-phone"></i><a href="tel:<?=$phone;?>"> <?=$phone;?></a>
       </div>
-  <!--     <div class="social-links float-right">
-        <a href="#" class="twitter"><i class="fa fa-twitter"></i></a>
-        <a href="#" class="facebook"><i class="fa fa-facebook"></i></a>
-        <a href="#" class="instagram"><i class="fa fa-instagram"></i></a>
-        <a href="#" class="google-plus"><i class="fa fa-google-plus"></i></a>
-        <a href="#" class="linkedin"><i class="fa fa-linkedin"></i></a>
-      </div> -->
+      <div class="social-links float-right">
+        <a href="https://facebook.com/gitstardigital" target="_blank" class="facebook"><i class="fa fa-facebook"></i></a>
+        <a href="https://twitter.com/GitstarD" class="twitter"><i class="fa fa-twitter"></i></a>
+        <a href="https://www.instagram.com/gitstardigital/" class="instagram"><i class="fa fa-instagram"></i></a>
+        <!-- <a href="#" class="google-plus"><i class="fa fa-google-plus"></i></a>
+        <a href="#" class="linkedin"><i class="fa fa-linkedin"></i></a> -->
+      </div> 
     </div>
   </section>
 
--- template/default/guest/index.php
+++ template/default/guest/index.php
@@ -136,7 +136,7 @@ $page_keywords = "website designers,web design,web design company,web design com
       <div class="container">
         <div class="section-header">
           <h2>Our Portfolio</h2>
-          <p>Sed tamen tempor magna labore dolore dolor sint tempor duis magna elit veniam aliqua esse amet veniam enim export quid quid veniam aliqua eram noster malis nulla duis fugiat culpa esse aute nulla ipsum velit export irure minim illum fore</p>
+          <p>We are proud of our solutions trust us, we build solutions not products...</p>
         </div>
 
         <div class="row portfolio-container">
